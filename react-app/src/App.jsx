7cf599e1138a643f799afefc08319d3454d18a90
K-Manju <arulmanju6383@gmail.com>
2026-04-18 18:00:40 +0530

    added the practice files

---
+++ react-app/src/App.jsx
@@ -0,0 +1,20 @@
+const Card = ({title}) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" />
+      <Card title="Avatar" />
+      <Card title="Titanic" />
+    </div>
+
+  )
+}
+
+export default App
